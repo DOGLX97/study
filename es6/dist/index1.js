7c316dfd711aa4f3d687b55f8b147d567b12bfb9
DOGLX97 <15663423507@163.com>
2017-10-18 19:05:08 +0800

    前端学习

---
+++ es6/dist/index1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = 5;
+var foo = function foo(x, y) {
+  return x + y;
+};
+var b = 5;
+var c = 5;
+var d = 5;
+var e = 5;
---
+++ es6/src/index1.js
@@ -0,0 +1,6 @@
+let a=5;
+let foo=(x,y)=>x+y;
+let b=5;
+let c=5;
+let d=5;
+let e=5;
