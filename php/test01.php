364046d8144f8d269ce37d8fd13387f2c9bba6f6
DOGLX97 <15663423507@163.com>
2017-11-07 19:36:27 +0800

    php学习

--- php/test01.php
+++ php/test01.php
@@ -16,7 +16,46 @@
     }
     echo "</table>";
 
+    echo "<br>";
 
+    echo "<table width='800px' border='1px'>";
+    for($i=9;$i>=0;$i--){
+        echo "<tr>";
+        for($j=1;$j<=$i;$j++){
+            echo "<td>$i*$j=".($i*$j)."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
 
+    echo "<br>";
+
+    echo "<table width='800px' border='1px'>";
+    for($i=1;$i<=9;$i++){
+        echo "<tr>";
+        for($k=1;$k<=9-$i;$k++){
+            echo "<td>&nbsp;</td>";
+        }
+        for($j=1;$j<=$i;$j++){
+            echo "<td>$i*$j=".($i*$j)."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    echo "<br>";
+
+    echo "<table width='800px' border='1px'>";
+    for($i=9;$i>=0;$i--){
+        echo "<tr>";
+        for($k=1;$k<=9-$i;$k++){
+            echo "<td>&nbsp;</td>";
+        }
+        for($j=1;$j<=$i;$j++){
+            echo "<td>$i*$j=".($i*$j)."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
 
 ?>
