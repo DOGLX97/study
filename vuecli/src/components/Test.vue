680ff4ee845c38fb01c8af4a88ef468f0f65eb8b
DOGLX97 <15663423507@163.com>
2017-11-19 11:02:10 +0800

    vuecli 学习

---
+++ vuecli/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="test">{{msg}}</h1>
+</template>
+<script>
+    export default{
+      data () {
+        return {
+          msg: 'Test'
+        }
+      }
+    }
+</script>
+<style scoped>
+.test{
+    background: #f0f;
+}
+</style>
